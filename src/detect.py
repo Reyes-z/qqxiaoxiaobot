7d3bcae7280117e878c7f516228a5ef336994b6b
Reyes <1745584170@qq.com>
2024-09-28 00:26:55 +0800

    更新到正式版本

--- src/detect.py
+++ src/detect.py
@@ -3,6 +3,7 @@ import time
 import pyautogui
 import pyperclip
 import random
+import json
 
 pos=[]
 alt = True
@@ -20,17 +21,21 @@ def end(e):
     print("共收集到", len(pos),"个坐标值")
     for i in range(len(pos)):
         print("第", i+1, "个坐标值:", pos[i] ,end='\n')
-
+    with open('pos.json', 'w') as file:
+        json.dump(pos, file)
+    print("成功将坐标存储到pos.json文件中")
+    
 keyboard.on_press_key("d", detect)
 keyboard.on_press_key("e", end)
 
 print("程序已启动，按d进行检测，按e停止。")
 
-while alt:
-    time.sleep(1)
-    mouse_x, mouse_y = pyautogui.position()
-    print("鼠标坐标:", mouse_x, mouse_y)    
-    
+while True:
+    if alt:
+        time.sleep(1)
+        mouse_x, mouse_y = pyautogui.position()
+        print("鼠标坐标:", mouse_x, mouse_y)    
+        
      
 
 
--- src/main.py
+++ src/main.py
@@ -3,17 +3,23 @@ import time
 import pyautogui
 import pyperclip
 import random
+import json
 from send import send_msg , wait_time
 
 pyautogui.FAILSAFE=False
 mode = -1
 pos = [(200,750),(1000,750),(1600,750)] # 这里就是所有QQ窗口对话框的坐标，按需更改。
 
+
+with open('pos.json', 'r') as file:
+    pos = json.load(file)
+print("成功载入pos文件中坐标信息。\n坐标数据为",pos)
+
 def start():
     print("开始执行")
     global mode
     print("1.自动修炼模式\n2.自动修炼自动突破模式\n3.修仙签到与宗门丹药领取\n4.自定义指令模式\n")
-    mode = int(input(" 请输入一个数字 (1, 2, 3):"))
+    mode = int(input(" 请输入一个数字(1, 2, 3, 4):"))
     print("你输入的数字是:", mode)
     print("若在运行过程中按空格，程序将下一轮操作前停止。")
 
